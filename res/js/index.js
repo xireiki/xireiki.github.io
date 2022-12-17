e17907952aaa59220de5d51971a4f70075cb9ed9
xireiki <xiayinlily@outlook.com>
2022-12-17 22:53:48 +0800

    Site updated: 2022-12-17 22:53:47

--- res/js/index.js
+++ res/js/index.js
@@ -5,36 +5,49 @@ function CopyText(text){
   if(!text){
     return
   };
-  if(!clip){
-    alert("您的浏览器不支持复制文本");
+  function copyToast(con, lv){
+    if(!con) return;
+    if(!lv) lv = 1;
+    if(lv == 2){
+      alert(con);
+    } else if(lv == 1) {
+      if(window.via){
+        window.via.toast(con);
+      } else {
+        $.toast(con);
+      }
+    }
+  }
+  function copy2(text){
+    let cb = document.createElement("button");
+    cb.id = "cb";
+    cb.style.width = "1px";
+    cb.style.height = "1px";
+    document.body.appendChild(cb);
+    let clipboardjs = new ClipboardJS("#cb", {
+      text: function(){
+        return text
+      }
+    });
+    clipboardjs.on("success", () => {
+      copyToast("已复制文本到剪贴板");
+      cb.remove();
+    });
+    clipboardjs.on("error", () => {
+      copyToast("已复制文本到剪贴板");
+      cb.remove();
+    });
+    document.getElementById("cb").click();
   }
   clip.writeText(text)
     .then(() => {
-      alert("已复制文本到剪贴板");
+      copyToast("已复制文本到剪贴板");
     })
     .catch(err => {
       if(err.name == "NotAllowedError"){
-        let cb = document.createElement("button");
-        cb.id = "cb";
-        cb.style.width = "1px";
-        cb.style.height = "1px";
-        document.body.appendChild(cb);
-        let clipboardjs = new ClipboardJS("#cb", {
-          text: function(){
-            return text
-          }
-        });
-        clipboardjs.on("success", () => {
-          // alert("已复制文本到剪贴板");
-          cb.remove();
-        });
-        clipboardjs.on("error", () => {
-          alert("复制失败,请检查您的浏览器设置");
-          cb.remove();
-        });
-        document.getElementById("cb").click();
+        copy2(text);
       } else {
-        alert("复制失败：" + err);
+        copyToast("复制失败：" + err);
       }
     });
 }
